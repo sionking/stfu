b14aaca63b0057350f8c40848d9a63c1900be2d6
patrickclery <patrick.clery@gmail.com>
2018-07-07 20:24:21 -0700

    - Added a .env config to login to Instagram - Running index.js follows @instagram

---
+++ mute.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+require('instagram-private-api')
+
+const Client = require('instagram-private-api').V1;
+const device = new Client.Device(process.env.IG_USER);
+const storage = new Client.CookieFileStorage(`./cookies/${process.env.IG_USER}.json`);
+
+Client.Session.create(
+  device,
+  storage,
+  process.env.IG_USER,
+  process.env.IG_PASSWORD
+).then(function (session) {
+  // Now you have a session, we can follow / unfollow, anything...
+  // And we want to follow Instagram official profile
+  return [session, Client.Account.searchForUser(session, 'instagram')]
+})
+  .spread(function (session, account) {
+    // return Client.Relationship.create(session, account.id);
+    return Client.Relationship.mute(session, account.id);
+  })
+  .then(function (relationship) {
+    console.log(relationship.params)
+    // {followedBy: ... , following: ... }
+    // Yey, you just followed @instagram
+  })
+
+
