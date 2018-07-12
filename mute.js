9665813f0f73cdf93d74d174c7ad06f46b274490
patrickclery <patrick.clery@gmail.com>
2018-07-11 22:46:39 -0700

    - This branch will use `kue` package for queuing jobs

--- mute.js
+++ mute.js
@@ -1,47 +1,68 @@
-require('dotenv').config()
-require('instagram-private-api')
+require('dotenv').config();
+require('instagram-private-api');
 
-_ = require('lodash')
-
+_ = require('lodash');
 
 const Client = require('instagram-private-api').V1;
 const device = new Client.Device(process.env.IG_USER);
 const storage = new Client.CookieFileStorage(`./cookies/${process.env.IG_USER}.json`);
 
-Client.Session.create(
+const getSession = async () => {
+  return Client.Session.create(
     device,
     storage,
     process.env.IG_USER,
     process.env.IG_PASSWORD
-  )
-  .then(async session => {
-    let accountId = await session.getAccountId()
-    let feeds = await new Client.Feed.AccountFollowing(session, accountId, 400);
-    feeds.get()
-      .then(res => {
+  );
+};
 
-        let kue = require('kue')
-          , queue = kue.createQueue();
+const init = async () => {
 
-        const followings = res.map(account => account._params.id)
+  // Login
+  const session = await getSession();
 
-        for (id of followings) {
-          Client.Account.muteStory(session, id)
-        }
+  // Get my account info
+  const myAccount = await session.getAccount();
 
-      })
-  })
+  // Part 1/2) Use my login and account info to get my followers
+  // Part 2/2) Call .get() on the feed object to
+  const followings = await (await new Client.Feed.AccountFollowing(session, myAccount.id)).get();
 
-// Get a list of all my followings
+  // Setup the queue
+  const queue = require('kue').createQueue();
+  queue.watchStuckJobs(1000);
+  const delay_in_ms = process.env.DELAY_SECONDS * 1000;
+  queue.on('ready', () => console.info('Queue is ready!'));
 
-// Loop through each
+  // Loop through the followings and get
+  let jobs = [];
 
-// Create a request to mute them
+  for (let account of followings.slice(0, process.env.MAX_MUTES)) {
 
-// If this account is in my `whiteList` array, I'll unmute() them (in case they were previously muted)
+    let job = queue.create('mute', account).save();
 
-// Otherwise, we call account.mute()
+    job.on('complete', result => {
+      console.log('Job completed with data ', result);
+    }).on('failed attempt', (errorMessage, doneAttempts) => {
+      console.log('Job failed');
+    }).on('failed', errorMessage => {
+      console.log('Job failed');
+    }).on('start', job => {
+      console.log(job);
+    }).on('job enqueue', (id, type) => {
+      console.log('Job %s got queued of type %s', id, type)``
+    });
 
-// .then() we hand off the request to our queueing module, so we don't get rate-limit banned by instagram
+    jobs.push(job)
 
+  }
 
+  for (job in jobs) {
+    console.log(`Processing ${job.data._params.username}`);
+  }
+  queue.process('mute', 1, (job, account) => {
+  });
+
+  // setTimeout(, process.env.DELAY_SECONDS * 1000)
+
+};
