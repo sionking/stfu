41a9ad468985964f0c5277c140beb7390e0f595f
patrickclery <patrick.clery@gmail.com>
2018-07-15 14:54:29 -0700

    - Supports a whitelist - Added a sample.env

--- mute.js
+++ mute.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 require('instagram-private-api');
 // FUNCTIONS
 const delay_in_ms = process.env.DELAY_SECONDS * 1000;
+const whitelist = process.env.WHITELIST.split(' ');
 
 const Client = require('instagram-private-api').V1;
 const device = new Client.Device(process.env.IG_USER);
@@ -21,7 +22,7 @@ queue.on('failed', (job, err) => {
   console.log(`FAILED: Mute ${job.data.username}: ${err.message}`);
 });
 queue.on('succeeded', job => {
-  console.log(`SUCCESS: muted ${job.data.username}!`)
+  console.log(`SUCCESS: Processed ${job.data.username}!`)
 });
 
 const muteAllUsers = async () => {
@@ -41,6 +42,7 @@ const muteAllUsers = async () => {
     feed.get().then(followings => {
 
       for (let f of followings
+        .reverse()
         .slice(0, process.env.MAX_MUTES)
         ) {
 
@@ -61,13 +63,51 @@ const muteAllUsers = async () => {
         .process(1, (job, done) => {
           console.log(`Started job ${job.id} for ${job.data.username}.`);
 
-          Client.Relationship.mutePosts(session, job.data.id);
-          Client.Relationship.muteStory(session, job.data.id);
+          Client.Relationship.get(session, job.data.id).then(rel => {
 
-          setTimeout(done, delay_in_ms)
+            if (whitelist.includes(job.data.username)) {
+
+              if (!rel.params.muting && !rel.params.is_muting_reel) {
+                console.log(`WHITELIST: ${job.data.username} is already whitelisted.`);
+                done();
+                return;
+              }
+
+              if (rel.params.muting) {
+                console.log(`WHITELIST: unmutePosts() for ${job.data.username}.`);
+                Client.Relationship.unmutePosts(session, job.data.id);
+              }
+              if (rel.params.is_muting_reel) {
+                console.log(`WHITELIST: unmuteStory() for ${job.data.username}.`);
+                Client.Relationship.unmuteStory(session, job.data.id);
+              }
+
+            } else {
+
+              // Check if the user is already muted
+              if (rel.params.muting && rel.params.is_muting_reel) {
+                console.log(`SKIP: ${job.data.username} is already muted both.`);
+                done();
+                return
+              }
+              if (!rel.params.muting) {
+                console.log(`Executing mutePosts() for ${job.data.username}.`);
+                Client.Relationship.mutePosts(session, job.data.id);
+              }
+              if (!rel.params.is_muting_reel) {
+                console.log(`Executing muteStory() for ${job.data.username}.`);
+                Client.Relationship.muteStory(session, job.data.id);
+              }
+              console.log(`*** Sleeping for ${process.env.DELAY_SECONDS} seconds.`)
+            }
+
+            // Sleep so we don't get rate limited
+            setTimeout(done, delay_in_ms)
+          })
+
         })
     })
-  })
+  });
 };
 
 muteAllUsers();
