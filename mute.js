3148d264353b92d1586e5b7b49991c37ccad66bf
patrickclery <patrick.clery@gmail.com>
2018-07-15 03:00:01 -0700

    - Queues jobs with delay

--- mute.js
+++ mute.js
@@ -1,68 +1,76 @@
 require('dotenv').config();
 require('instagram-private-api');
-
-_ = require('lodash');
+// FUNCTIONS
+const delay_in_ms = process.env.DELAY_SECONDS * 1000;
 
 const Client = require('instagram-private-api').V1;
 const device = new Client.Device(process.env.IG_USER);
 const storage = new Client.CookieFileStorage(`./cookies/${process.env.IG_USER}.json`);
 
-const getSession = async () => {
-  return Client.Session.create(
-    device,
-    storage,
-    process.env.IG_USER,
-    process.env.IG_PASSWORD
-  );
+// *** QUEUE ***
+
+const Queue = require('bee-queue');
+const queue = new Queue('mute');
+queue.destroy().then(() => console.log('Queue destroyed!'));
+
+queue.on('ready', () => console.log('Ready!'));
+queue.on('error', (err) => {
+  console.log(`A queue error happened: ${err.message}`);
+});
+queue.on('failed', (job, err) => {
+  console.log(`Job ${job.id} failed with error ${err.message}`);
+});
+queue.on('succeeded', job => {
+  console.log(`Job succeed for muting user ${job.data.username}`)
+});
+
+queue.checkHealth().then(counts => console.log(counts));
+
+const muteAllUsers = async () => {
+
+  const session = await new Client.Session
+    .create(
+      device,
+      storage,
+      process.env.IG_USER,
+      process.env.IG_PASSWORD
+    );
+
+  session.getAccountId().then(id => {
+
+    let feed = new Client.Feed.AccountFollowing(session, id);
+
+    feed.get().then(followings => {
+
+      for (let f of followings
+        .reverse()
+        .slice(0, process.env.MAX_MUTES)
+        ) {
+
+        console.log(`Creating a job for ${f._params.username}.`);
+
+        queue.createJob({
+          username: f._params.username,
+          id: f._params.id
+        })
+          .save()
+          .then(job => {
+            console.log(`Saved job ${job.id} for ${job.data.username}`)
+          })
+
+      }
+
+      queue
+        .process(1, (job, done) => {
+          console.log(`Processing job ${job.id} for ${job.data.username}.`);
+
+          Client.Relationship.mutePosts(session, job.data.id).then(() => {
+            setTimeout(done, delay_in_ms)
+          })
+        })
+    })
+  })
 };
 
-const init = async () => {
+muteAllUsers();
 
-  // Login
-  const session = await getSession();
-
-  // Get my account info
-  const myAccount = await session.getAccount();
-
-  // Part 1/2) Use my login and account info to get my followers
-  // Part 2/2) Call .get() on the feed object to
-  const followings = await (await new Client.Feed.AccountFollowing(session, myAccount.id)).get();
-
-  // Setup the queue
-  const queue = require('kue').createQueue();
-  queue.watchStuckJobs(1000);
-  const delay_in_ms = process.env.DELAY_SECONDS * 1000;
-  queue.on('ready', () => console.info('Queue is ready!'));
-
-  // Loop through the followings and get
-  let jobs = [];
-
-  for (let account of followings.slice(0, process.env.MAX_MUTES)) {
-
-    let job = queue.create('mute', account).save();
-
-    job.on('complete', result => {
-      console.log('Job completed with data ', result);
-    }).on('failed attempt', (errorMessage, doneAttempts) => {
-      console.log('Job failed');
-    }).on('failed', errorMessage => {
-      console.log('Job failed');
-    }).on('start', job => {
-      console.log(job);
-    }).on('job enqueue', (id, type) => {
-      console.log('Job %s got queued of type %s', id, type)``
-    });
-
-    jobs.push(job)
-
-  }
-
-  for (job in jobs) {
-    console.log(`Processing ${job.data._params.username}`);
-  }
-  queue.process('mute', 1, (job, account) => {
-  });
-
-  // setTimeout(, process.env.DELAY_SECONDS * 1000)
-
-};
